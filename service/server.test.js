53a21c8138c029a019a3fc9f872ea1d538d00e54
LucijaBa <lb52002@fer.hr>
2022-05-22 23:42:16 +0200

    update

--- service/server.test.js
+++ service/server.test.js
@@ -1,5 +1,8 @@
 const request = require('supertest');
 const app = require('./server');
+const VrstaTrening = require('./models/VrstaTrening');
+const VrstaTreningRepo = require('./models/VrstaTreningRepository');
+const Termin = require('./models/Termin');
 
 describe('Todos API', () => {
     it('GET /treninzi ---> polje treninga', () => {
@@ -23,7 +26,7 @@ describe('Todos API', () => {
    
    it('GET /trening ---> određeni trening', () => {
         return request(app)
-            .get('/trening/?id=1')
+            .get('/trening?id=1')
             .expect(200)
             .then((response) => {
                 expect(response.body).toEqual(
@@ -39,7 +42,7 @@ describe('Todos API', () => {
     
     it('GET /trening ---> 404', () => {
         return request(app)
-            .get('/trening/?id=9999')
+            .get('/trening?id=9999')
             .expect(404)
     });
 
@@ -76,4 +79,70 @@ describe('Todos API', () => {
             )
         })
     });
+
+    it('GET /termini ---> termini jednog treninga', () => {
+        return request(app)
+            .get('/termini?trening=2')
+            .expect(200)
+    });
+
+    it('POST /obrisiTrening ---> brisanje nepostojećeg treninga', () => {
+        return request(app)
+            .post('/obrisiTrening')
+            .send({
+               id: 999
+            })
+            .expect(400)
+    });
+
+    it('POST /obrisiVrstuTreninga ---> brisanje nepostojeće vrste treninga', () => {
+        return request(app)
+            .post('/obrisiVrstuTreninga')
+            .send({
+               id: 999
+            })
+            .expect(400)
+    });
+
+
+    it('POST /urediTermin ---> uređivanje nepostojećeg termina', () => {
+        return request(app)
+            .post('/urediTermin')
+            .send({
+               id: 999
+            })
+            .expect(400)
+    });
+
+/**    it('POST /urediVrstuTreninga ---> uređivanje vrste treninga', () => {
+        return request(app)
+            .post('/urediVrstuTreninga')
+            .send({
+               id: 2,
+               naziv: ''
+            })
+            .expect(200)
+    }); */
+
+
+})
+
+describe('Testiranje funkcija', () => {
+    it('dodavanje nove vrste treninga', () => {
+        const novaVrsta = new VrstaTrening(5, 'boks');
+        expect(novaVrsta.naziv).toEqual('boks');
+        expect(novaVrsta.id).toEqual(5);
+    });
+
+    it('dodaje novu vrstu', () => {
+        const novaVrsta = new VrstaTrening(null, '');
+        expect(novaVrsta).toEqual({id:null, naziv:''});
+    });
+
+    it('dodavanje novog termina', () => {
+        const noviTermin = new Termin(25, 1, '20-5-2022', '20:00:00');
+        expect(noviTermin).toEqual({id:25, trener:1, datum:'20-5-2022', vrijeme:'20:00:00'});
+    });
+
+
 })
