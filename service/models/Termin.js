e43cfa41848f09c1954de050887c06134c82ed35
Gordana Borotić <gordana.borotic@fer.hr>
2022-05-22 19:23:50 +0200

    update

--- service/models/Termin.js
+++ service/models/Termin.js
@@ -38,7 +38,19 @@ module.exports = class Termin {
 
     static async changeTrainer(termin, trener){
         try{
-            const sql = `UPDATE termin SET idTrener = ` + trener + ` WHERE id = ` + termin;
+            let sql = `SELECT * FROM termin where id = ` + termin
+            let result = await db.query(sql);
+            let datum = result.rows[0].datum
+            let vrijeme = result.rows[0].vrijeme
+            sql = `SELECT * FROM termin WHERE  idTrener = ` + trener + ` AND datum = (SELECT datum from termin where id = `+ termin + `) AND vrijeme < '` + vrijeme + `'::TIME AND vrijeme + '1 hour'::INTERVAL > '` + vrijeme + `'::TIME`
+            result = await db.query(sql);
+            if(result.rowCount >=1)
+                return false
+            sql = `SELECT * FROM termin WHERE idTrener = ` + trener + ` AND datum =  (SELECT datum from termin where id = `+ termin + `)  AND vrijeme > '` + vrijeme + `'::TIME AND vrijeme - '1 hour'::INTERVAL < '` + vrijeme + `'::TIME`
+            result = await db.query(sql);
+            if(result.rowCount >=1)
+                return false
+            sql = `UPDATE termin SET idTrener = ` + trener + ` WHERE id = ` + termin;
             await db.query(sql);
             return true
         }catch(err){
